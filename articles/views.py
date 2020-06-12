6639e84d171f252f0447e7784a54de8b02eaa10f
lakshyatm <58063296+lakshyatm@users.noreply.github.com>
2020-06-12 14:56:01 +0530

    Update views.py

--- articles/views.py
+++ articles/views.py
@@ -9,12 +9,12 @@ from django.contrib.auth.models import User
 # Create your views here.
 def article_list(request):
     articles = Article.objects.all().order_by('date')
-    return render(request, "articles/article_list.html",{'articles':articles})
+    return render(request, 'articles:article_list.html' ,{'articles':articles})
 
 def article_detail(request,slug):
     # return HttpResponse(slug)
     article = Article.objects.get(slug=slug)
-    return render(request, 'articles/article_detail.html',{'article':article})
+    return render(request, 'articles:article_detail.html',{'article':article})
 
 @login_required(login_url="/accounts/login/")
 def article_create(request):
