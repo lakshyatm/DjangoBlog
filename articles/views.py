547e4850cf4599530d7756fe327ff89f1a4b1d9f
lakshyatm <58063296+lakshyatm@users.noreply.github.com>
2020-06-16 23:02:34 +0530

    Update views.py

--- articles/views.py
+++ articles/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render,redirect, get_object_or_404
-from .models import Article, Comment
+from .models import Article
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 from . import forms
