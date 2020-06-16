a802e6cc796c2a88348ff193496806151bf9dda5
lakshyatm <58063296+lakshyatm@users.noreply.github.com>
2020-06-16 22:57:32 +0530

    Update views.py

--- articles/views.py
+++ articles/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render,redirect, get_object_or_404
-from .models import Article
+from .models import Article, Comment
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 from . import forms
@@ -9,24 +9,12 @@ from django.contrib.auth.models import User
 # Create your views here.
 def article_list(request):
     articles = Article.objects.all().order_by('date')
-    html = "<html><body>    <h1>Article List</<h1>
-    <div class="articles">
-      {% for article in articles %}
-        <div class="article">
-          <h2><a href="{% url 'articles:detail' slug=article.slug %}">{{article.title}}</a></h2>
-          <p>{{article.snippet}}</p>
-          <p>{{article.date}}</p>
-          <p class="author">added by {{article.author.username}}</p>
-
-        </div>
-      {% endfor %}
-    </div></body></html>"
-    return HttpResponse(html)
+    return render(request, "articles/article_list.html",{'articles':articles})
 
 def article_detail(request,slug):
     # return HttpResponse(slug)
     article = Article.objects.get(slug=slug)
-    return render(request, 'article_detail.html',{'article':article})
+    return render(request, 'articles/article_detail.html',{'article':article})
 
 @login_required(login_url="/accounts/login/")
 def article_create(request):
@@ -39,9 +27,19 @@ def article_create(request):
             return redirect('articles:list')
     else:
         form = forms.CreateArticle()
-    return render(request,'article_create.html',{'form':form})
+    return render(request,'articles/article_create.html',{'form':form})
 
 def LikeView(request):
     post = get_object_or_404(User, id=request.POST.get('post_id'))
     post.likes.add(request.user)
     return HttpResponseRedirect(reverse('article_detail',args=[str(pk)]))
+
+def post_detail(request, id, slug):
+    post = get_object_or_404(Article, id=id, slug=slug)
+    comments = Comment.objects.filter(post=post).order_by('-id')
+
+    context = {
+        'post' : post,
+        'comments' : comments,
+    }
+    return render(request, 'articles/article_detail', context)
